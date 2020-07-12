fa1e30fbf17cd12006f24e64727caeb14b5cc933
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-07-12 11:53:08 +0200

    better method for sort-desc found and implemented

--- sorting/sort-desc.py
+++ sorting/sort-desc.py
@@ -1,6 +1,6 @@
 def is_sorted(lst, key=lambda x: x):
     for i, el in enumerate(lst[1:]):
-        if key(el) < key(lst[i]):
+        if key(el) > key(lst[i]):
             return False
     return True
 
@@ -8,15 +8,10 @@ def sort_desc(lst):
     i = 0
     while not is_sorted(lst):
         try:
-            if lst[i] > lst[i+1]: lst[i], lst[i+1] = lst[i+1], lst[i]
+            if lst[i] < lst[i+1]: lst[i], lst[i+1] = lst[i+1], lst[i]
             else: pass
             i += 1
         except IndexError:
             i = 0
-
-    lst_desc = []
-    for el in lst:
-        lst_desc.insert(0, el)
-    return lst_desc
-
+    return lst
 print(sort_desc([1,4,6,2,3,6,1]))
